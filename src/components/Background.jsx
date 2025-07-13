91e358ec161f0899f5d17193a5234c7ab1765541
Himal Subedi <itissubedi@gmail.com>
2025-07-13 15:41:08 +0545

    Updated Readme

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,6 +1,6 @@
 function Background({children}){
     return(
-        <div className= 'bg-red-500 min-h-screen w-full'>
+        <div className= 'bg-sky-500 min-h-screen w-full'>
             {children}
 
         </div>
