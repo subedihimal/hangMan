828b54730a730023b61de563c737ac4693a79b5e
Himal Subedi <itissubedi@gmail.com>
2025-07-16 10:05:36 +0545

    feat retrive word and hint in singleplayer from json server

--- src/pages/SinglePlayer.jsx
+++ src/pages/SinglePlayer.jsx
@@ -1,8 +1,28 @@
-function SinglePlayer(){
- return(
-    <>
-    <h1>Single Player</h1>
-    </>
- )   
+import { useEffect } from "react";
+
+function SinglePlayer() {
+
+   function getWords() {
+
+      async function fetchWords() {
+         const response = await fetch('http://localhost:3000/words');
+         const allWords = await response.json();
+         console.log(allWords);
+
+         const word = Math.floor(Math.random() * allWords.length);
+         console.log(allWords[word]);
+      }
+
+      useEffect(() => {
+         fetchWords();
+      }, [])
+   }
+   getWords();
+
+   return (
+      <>
+         <h1>Single Player</h1>
+      </>
+   )
 }
 export default SinglePlayer;
